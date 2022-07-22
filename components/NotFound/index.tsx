5e9f4beb658cc284de3802e6af9af52cae9bbc2d
ThunderRedStar <wvongphanith30@stuy.edu>
2022-07-22 13:25:30 -0400

    404 nicer

--- components/NotFound/index.tsx
+++ components/NotFound/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
-const NotFound: React.FC = () => <h1>404 - Page Not Found</h1>;
+const NotFound: React.FC = () => {
+    <><h1>404</h1><p>You have reached a page that doesn't exist (yet). Would you like to go back?</p>
+        <button onClick={() => window.history.back()}>Go back</button>
+    </>
+};
 
 export default NotFound;
